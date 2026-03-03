3b43f1e302c4d959e3bb6fc8dee155712d59233c
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-03-03 16:33:46 +0700

    implementasi prosedur & fungsi

--- praktikum3_3.cpp
+++ praktikum3_3.cpp
@@ -3,3 +3,11 @@
 using namespace std;
 //Deklarasi Global
     float p,l;
+
+//Implementasi Prosedur & Fungsi
+void Input() {
+    cout << "Masukkan sisi Panjang : ";
+    cin >> p;
+    cout << "Masukkan sisi Lebar : ";
+    cin >> l;
+}
